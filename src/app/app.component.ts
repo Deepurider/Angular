593285a5c45f016c2d0d0eb7adccae9387a9cd2c
Deepurider <pateldeep691996@gmail.com>
2022-05-24 21:04:12 +0530

    Add datagrid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project1';
+
+  public colDefs:ColDef[] = [
+    {headerName:'username' , field:'username'},
+    {headerName:'password' , field:'password'},
+    {headerName:'email' , field:'email'}
+  ]
+
+  public rows = [
+    {username:'Deepurider1' , password:'deep@123' , email:'pateldeep691996@gmail.com'},
+    {username:'Deepurider1' , password:'deep@123' , email:'pateldeep691996@gmail.com'},
+    {username:'Deepurider1' , password:'deep@123' , email:'pateldeep691996@gmail.com'},
+  ]
 }
