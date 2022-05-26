54c0e56af4146b2e172c9366f4800ddba47710bb
Deepurider <pateldeep691996@gmail.com>
2022-05-26 21:09:55 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,18 @@
-import { Component } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project1';
-
-  public colDefs:ColDef[] = [
-    {headerName:'username' , field:'username'},
-    {headerName:'password' , field:'password'},
-    {headerName:'email' , field:'email'}
-  ]
-
-  public rows = [
-    {username:'Deepurider1' , password:'deep@123' , email:'pateldeep691996@gmail.com'},
-    {username:'Deepurider1' , password:'deep@123' , email:'pateldeep691996@gmail.com'},
-    {username:'Deepurider1' , password:'deep@123' , email:'pateldeep691996@gmail.com'},
-  ]
+export class AppComponent implements OnInit {
+  constructor(
+    public toastr:ToastrService
+  ){}
+  ngOnInit() {
+    setTimeout(() => {
+      this.toastr.success('After 10 second' , 'status');
+    }, 10000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgGridModule } from 'ag-grid-angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AgGridModule
+    AgGridModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
