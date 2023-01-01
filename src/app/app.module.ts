d357ae8c7a25f27ff8ad8259676847adc59de7f8
Deep Patel <pateldeep691996@gmail.com>
2023-01-01 21:15:18 +0530

    Implement home module and setup routing for other fuctionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgGridModule } from 'ag-grid-angular';
@@ -6,17 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ComponentProjectionComponent } from './features/component-projection/component-projection.component';
-import { Route, Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { MatMaterialModule } from './shared/module/mat-material.module';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    ComponentProjectionComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,26 +19,9 @@ import { MatMaterialModule } from './shared/module/mat-material.module';
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
     HttpClientModule,
-    MatMaterialModule
+    MatMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-// // for custom login for routes
-// export function initializeAppCustomLogin(router: Router, http: HttpClient): () => Promise<void> {
-//   return () => new Promise((resolve) => {
-//     console.log('*************** Loading Dynamic Routes ***************');
-//     http.get('../assets/data/menu.json').subscribe((res: any) => {
-//       const routes: Route[] = [];
-//       res.forEach((element: any) => {
-//         routes.push({ path: element.url.toString(), component: ComponentProjectionComponent });
-//       });
-//       router.resetConfig(routes);
-//       setTimeout(() => {
-//         console.log("********* All Routes Iniliaztion completed *********");
-//         resolve();
-//       }, 3000);
-//     });
-//   });
-// }
+export class AppModule {}
